694b30e0b6807fe6452bcb0ead0d1fa53797779d
StoryBeeBooks <support@storybee.space>
2025-11-23 11:45:18 -0500

    Add SEO-friendly FAQ page and footer link for Grant Officers & Partners

--- js/footer.js
+++ js/footer.js
@@ -38,6 +38,7 @@ class Footer {
                             <li><a href="ar-research.html" class="footer-link">AR Technology</a></li>
                             <li><a href="learning-programs.html" class="footer-link">Learning Programs</a></li>
                             <li><a href="semiconductor-research.html" class="footer-link">Semiconductor Research</a></li>
+                            <li><a href="faq.html" class="footer-link">FAQ for Grant Officers & Partners</a></li>
                         </ul>
                     </div>
                     
